39f41c4d17d6cd8b782c8e95ce06dea1bf2842a4
Radu Necsanu <necsanu_radu@yahoo.com>
2021-09-30 16:48:25 +0100

    RN first and last commit no tests

---
+++ src/gilded_rose.js
@@ -0,0 +1,53 @@
+class Shop {
+  constructor(items = []) {
+    this.items = this.fitInMinMax(items);
+  }
+
+  updateQuality() {
+    this.items.forEach((item, index) => {
+      this.reduceSellIn(index, item);
+      this.calculateQuality(index, item);
+    });
+    return this.fitInMinMax(this.items);
+  }
+
+  reduceSellIn(index, item) {
+    this.items[index].sellIn = item.sellIn - 1;
+  }
+
+  calculateQuality(index, item) {
+    let p = this.getItemParameters(this.items[index]);
+    if (item.sellIn >= 0)
+      this.items[index].quality = item.quality + this.returnValidPase(p, item);
+    else
+      this.items[index].quality =
+        item.quality * p.expiredMultiplier + p.expireQualityPase;
+  }
+
+  returnValidPase(p, item) {
+    p = p.dayToDayQualityPase.filter((dayPase) => {
+      return dayPase.startDay >= item.sellIn && dayPase.endDay <= item.sellIn
+        ? true
+        : false;
+    });
+    p.unshift({ pase: 1 });
+    return p[p.length - 1].pase;
+  }
+
+  getItemParameters(item) {
+    let itemParameters;
+    for (let param of parameters) {
+      itemParameters = param;
+      if (item.name.match(new RegExp("^" + param.matcher, "i"))) break;
+    }
+    return itemParameters;
+  }
+
+  fitInMinMax(items) {
+    return items.map((item) => {
+      if (!item.name.match(new RegExp("^Sulfuras", "i")))
+        item.quality = Math.max(Math.min(Number(item.quality), 50), 0);
+      return item;
+    });
+  }
+}
