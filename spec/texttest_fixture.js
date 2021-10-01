c5249adb1125d8acc63fc1b688bcf4975866827e
Radu Necsanu <71831322+necsanuradu@users.noreply.github.com>
2021-10-01 16:39:42 +0100

    Update texttest_fixture.js

--- spec/texttest_fixture.js
+++ spec/texttest_fixture.js
@@ -11,15 +11,6 @@ const itemsWorkingOld = [
   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
 ];
 
-const items = [
-  new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
-  new Item("Elixir of the Mongoose", 5, 7),
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-];
+const items = itemsWorkingOld.slice();
 
 const itemsNewFeature = [new Item("Conjured Mana Cake", 3, 16)];
